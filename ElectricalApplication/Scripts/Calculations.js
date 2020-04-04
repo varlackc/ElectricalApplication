1fe0343a5f5e0cad0fc164955d27eae4b36418b0
varlackc <varlackc@mail.gvsu.edu>
2020-04-03 22:54:45 -0500

    Update Calculations.js

--- ElectricalApplication/Scripts/Calculations.js
+++ ElectricalApplication/Scripts/Calculations.js
@@ -72,6 +72,7 @@ var DistanceFormula3D = function (x1, y1, z1, x2, y2, z2) {
 //Pythagorean Identity -- X^2 + Y^2 = Z^2
 var FindHypothenuse = function (x, y) {
 
+    return;
 };
 
 //Trigonometric Identities
