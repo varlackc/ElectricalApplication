bed51ab02816b6bf875e274171e6a3c35ab6f8dd
varlackc <varlackc@mail.gvsu.edu>
2020-04-03 23:00:25 -0500

    Update Calculations.js

--- ElectricalApplication/Scripts/Calculations.js
+++ ElectricalApplication/Scripts/Calculations.js
@@ -59,7 +59,7 @@ var PowerAmperageFindVoltage = function (power, amperage) {
 //2D Distance
 var DistanceFormula2D = function (x1, y1, x2, y2) {
     var distance = sqrt((x2 - x1)*(x2 - x1) + (y2 - y1)*(y2 - y1));
-    return;
+    return distance;
 };
 
 //3D distance
