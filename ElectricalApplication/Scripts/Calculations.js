4d769b4d6064cbab3ddd61c39132f250cfcd046b
varlackc <varlackc@mail.gvsu.edu>
2020-04-07 23:15:28 -0500

    Update Calculations.js

--- ElectricalApplication/Scripts/Calculations.js
+++ ElectricalApplication/Scripts/Calculations.js
@@ -121,7 +121,7 @@ var StandardDeviation = function () {
 //Kinematics
 //find final velocity Vf = V0 + (A)*(T)
 var VelocityFinalGivenV0AT = function () {
-
+    return;
 };
 //find final position
 //find acceleration
