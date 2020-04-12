82251aa31b045143430e7cbd5f3d27c2fee6c72d
varlackc <varlackc@mail.gvsu.edu>
2020-04-11 21:19:52 -0500

    Update Calculations.js

--- ElectricalApplication/Scripts/Calculations.js
+++ ElectricalApplication/Scripts/Calculations.js
@@ -113,7 +113,7 @@ var Mean = function (ArrayValue) {
 };
 //Mean
 var Median = function (ArrayValue) {
-    var ArraySize = 0;
+    var ArraySize = ArrayValue.Length();
     var result = 0;
     return result;
 };
