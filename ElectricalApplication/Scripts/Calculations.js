c288ec5ae36f3354692fdefa787fd2556e525dac
varlackc <varlackc@mail.gvsu.edu>
2020-04-11 21:15:59 -0500

    Update Calculations.js

--- ElectricalApplication/Scripts/Calculations.js
+++ ElectricalApplication/Scripts/Calculations.js
@@ -11,7 +11,7 @@
 
 // Declare variables
 var Resistance = 0; // this indicates the resistance of an electrical or electronic component
-var Voltage = 0;
+var Voltage = 0; // Voltage of an electrical or electronic component
 var Amperage = 0;
 var Power = 0;
 var Energy = 0;
