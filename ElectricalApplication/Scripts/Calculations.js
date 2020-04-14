5f60a37141a37de55c1481f5159fdd1e19757c27
varlackc <varlackc@mail.gvsu.edu>
2020-04-13 23:48:47 -0500

    Update Calculations.js

--- ElectricalApplication/Scripts/Calculations.js
+++ ElectricalApplication/Scripts/Calculations.js
@@ -74,6 +74,7 @@ var DistanceFormula2D = function (x1, y1, x2, y2) {
 };
 
 //3D distance
+// Inputs: (x1, y1 and z1) (x2, y2, and z2)
 var DistanceFormula3D = function (x1, y1, z1, x2, y2, z2) {
     var distance = sqrt((x2 - x1) * (x2 - x1) + (y2 - y1) * (y2 - y1) + (z2 - z1) * (z2 - z1));
     return distance;
