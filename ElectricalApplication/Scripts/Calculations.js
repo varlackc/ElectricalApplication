3f65a170fb8d97f9d2f2e6c6a31bfef3744045ae
varlackc <varlackc@mail.gvsu.edu>
2020-04-14 23:30:14 -0500

    Update Calculations.js

--- ElectricalApplication/Scripts/Calculations.js
+++ ElectricalApplication/Scripts/Calculations.js
@@ -154,7 +154,7 @@ var VelocityFinalGivenV0AT = function (V0, A, T) {
     return result;
 };
 
-//find final position
+// find final position
 //find acceleration
 //find total energy of the system
 // find momentum
