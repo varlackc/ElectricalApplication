1df15fce7755d41b16952e22b839ff6155753329
varlackc <varlackc@mail.gvsu.edu>
2020-04-24 23:38:37 -0500

    Update HomeController.cs

--- ElectricalApplication/Controllers/HomeController.cs
+++ ElectricalApplication/Controllers/HomeController.cs
@@ -16,14 +16,5 @@ namespace ElectricalApplication.Controllers
         }
         //----------------------
 
-        // -- About section --
-        public ActionResult About()
-        {
-            ViewBag.Message = "Your application description page.";
-
-            return View();
-        }
-        //---------------------
-
     }
 }
