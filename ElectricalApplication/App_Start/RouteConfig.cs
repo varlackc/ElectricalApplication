3f90fcd224d57bbc734cef8d355412c48d93c912
varlackc <varlackc@mail.gvsu.edu>
2020-04-24 23:36:50 -0500

    Update RouteConfig.cs

--- ElectricalApplication/App_Start/RouteConfig.cs
+++ ElectricalApplication/App_Start/RouteConfig.cs
@@ -7,6 +7,7 @@ using System.Web.Routing;
 
 namespace ElectricalApplication
 {
+    // Configure the routing on the application, determine the default location 
     public class RouteConfig
     {
         public static void RegisterRoutes(RouteCollection routes)
