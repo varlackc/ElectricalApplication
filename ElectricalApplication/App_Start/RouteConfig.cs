fc38807fa380e91ce35c544e9b7e678ad1578289
varlackc <varlackc@mail.gvsu.edu>
2020-08-11 23:45:10 -0500

    Update RouteConfig.cs

--- ElectricalApplication/App_Start/RouteConfig.cs
+++ ElectricalApplication/App_Start/RouteConfig.cs
@@ -13,7 +13,6 @@ namespace ElectricalApplication
         public static void RegisterRoutes(RouteCollection routes)
         {
             routes.IgnoreRoute("{resource}.axd/{*pathInfo}");
-
             routes.MapRoute(
                 name: "Default",
                 url: "{controller}/{action}/{id}",
