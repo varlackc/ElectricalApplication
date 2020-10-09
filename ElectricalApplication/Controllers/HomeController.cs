d6258a26743a10bcf99788b1cb3851fbc5e409a7
Maxwell Varlack <varlackc@mail.gvsu.edu>
2020-10-08 22:20:42 -0700

    Update HomeController.cs

--- ElectricalApplication/Controllers/HomeController.cs
+++ ElectricalApplication/Controllers/HomeController.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using System.Diagnostics;
 
 //Electrical application
 namespace ElectricalApplication.Controllers
@@ -12,6 +13,10 @@ namespace ElectricalApplication.Controllers
         // ----- Main Page -----
         public ActionResult Index()
         {
+            // Display in the debugging section
+            Debug.WriteLine("\n--------------------------");
+            Debug.WriteLine("| Made it to this section");
+            Debug.WriteLine("--------------------------\n");
             return View();
         }
         //----------------------
