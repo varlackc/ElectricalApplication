26994b6be6a5868759fa580c78e1b07ab02a1c4e
Maxwell Varlack <varlackc@mail.gvsu.edu>
2020-10-10 22:06:37 -0700

    Update ElectricalData.cs

--- ElectricalApplication/Models/ElectricalData.cs
+++ ElectricalApplication/Models/ElectricalData.cs
@@ -8,5 +8,9 @@ namespace ElectricalApplication.Models
     public class ElectricalData
     {
         
+  public string Subject {get; set; }
+  public string StudentName {get; set;}
+  public string StudentLastName{get; set;}
+
     }
 }
